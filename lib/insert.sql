e92bd4b7546a9263dfaee7a590588bafcb345064
Leslie Kogi <lesliekogi@gmail.com>
2025-05-22 17:26:23 +0300

    organises bears

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 4, 'M','Brown', 'Calm', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy',5 ,'F','Brown','Timid',0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha',7, 'F','Brown','Agressive',1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergent Brown',5, 'M','Brown','Clam',0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa',7, 'F','Brown','Agressive',1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch',9, 'M','Brown','Timid',0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy',3, 'M','Brown','Agressive',1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ( NULL, 2, 'F','Brown','Calm',0);
